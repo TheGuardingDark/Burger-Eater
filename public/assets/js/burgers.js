ec4dd7886517215569f1342d8892b4b7c95ca3d8
Scout <therearespidersinthisemail@gmail.com>
2019-07-27 17:37:48 -0600

    First commit

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,37 @@
+$(function() {
+    $(".eatBurger").on("click", function(event) {
+        var id = $(this).data("burgersid");
+        var eaten = {
+            devoured: true
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: eaten
+        }).then(
+            function() {
+                console.log("Burger has been eaten");
+                location.reload();
+            }
+        ); 
+    });
+
+$(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+        name: $("#burg").val().trim(),
+        devoured: 0
+    };
+
+    $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+    }).then(
+        function() {
+            console.log("Created new Burger.");
+            location.reload();
+        }
+    );
+});
+})
