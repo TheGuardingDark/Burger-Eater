fc59ab36789e795854cf11050df04291e70794c5
Scout <therearespidersinthisemail@gmail.com>
2019-07-28 18:28:19 -0600

    tried taking port info out

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,13 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "Houdini1!",
     database: "burger_db",
   });
-}
+};
 
 // Make connection.
 connection.connect(function(err) {
